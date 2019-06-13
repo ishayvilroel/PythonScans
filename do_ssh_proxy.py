b89fd608a4992c1c308432312100c4f35bf0ea4c
IshVil <51756588+ishayvilroel@users.noreply.github.com>
2019-06-13 09:28:12 +0300

    Update do_ssh_proxy.py

--- do_ssh_proxy.py
+++ do_ssh_proxy.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015, LCI Technology Group, LLC
-# All rights reserved.
-#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
